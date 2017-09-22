1af604577481c85448d1a51ed05c86778825b319
sme_admin <dhafir.fuller@gmail.com>
2017-09-22 17:56:59 -0400

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
